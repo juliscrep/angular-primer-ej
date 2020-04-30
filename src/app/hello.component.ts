e55bfe10f46f038471ccd25f6451a809dfe9e89e
juliscrep <julietascrepnik@gmail.com>
2020-04-30 15:37:22 -0300

    1 file modified, 3 added

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { SubscriptionLoggable } from 'rxjs/internal/testing/SubscriptionLoggable';
 
 @Component({
   selector: 'hello',
@@ -7,4 +8,5 @@ import { Component, Input } from '@angular/core';
 })
 export class HelloComponent  {
   @Input() name: string;
+  
 }
